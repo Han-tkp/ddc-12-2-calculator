2bb4aa4bfddab41a97519d8d3795c76222bbf1d3
Han <h4nnyza@gmail.com>
2026-03-11 06:50:46 +0700

    Edit name location ddc: VBDC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default async function Home() {
             </div>
             <div>
               <h1 className="font-bold text-lg md:text-xl text-slate-800 tracking-tight leading-tight">
-                DDC 12.2
+                VBDC 12.2
               </h1>
               <p className="text-[10px] md:text-xs text-slate-500 font-medium hidden sm:block">
                 ศูนย์ควบคุมโรคติดต่อนำโดยแมลงที่ 12.2 สงขลา
